8a5d2555e287dfee2f378f03daf99456814ee44d
iwtgafu <iwtgafu@gmail.com>
2017-06-16 19:28:43 +0200

    1

--- src/server.js
+++ src/server.js
@@ -87,6 +87,20 @@ app.get('/api/ryanair/pricelist', function (req, res) {
   })
 });
 
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
 
 // error handling
 app.use(function(err, req, res, next){
